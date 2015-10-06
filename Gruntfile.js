5b6fee889b1ad9c29ca686ace22f79ed0826893c
System Administrator <justinkheisler@gmail.com>
2015-10-06 17:17:47 -0300

    adding json files to Gruntfile.js so they get included in the dist folder on build

--- Gruntfile.js
+++ Gruntfile.js
@@ -62,7 +62,7 @@ module.exports = function (grunt) {
         files: [
           '<%= yeoman.app %>/{,*/}*.html',
           '.tmp/styles/{,*/}*.css',
-          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}'
+          '<%= yeoman.app %>/images/{,*/}*.{png,jpg,jpeg,gif,webp,svg}',
         ]
       }
     },
@@ -357,6 +357,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
+            '*.json',
             '*.html',
             'images/{,*/}*.{webp}',
             'styles/fonts/{,*/}*.*'
