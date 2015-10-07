2ce5ae8ee8f7000eb8888ef90512d5e76b8926e2
System Administrator <justinkheisler@gmail.com>
2015-10-07 19:41:46 -0300

    adding wiggles and horizons to demo, fixed some issues and formatting

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -22,6 +22,54 @@ angular.module('visualizrApp')
 				.draw();
 		});
 
+		$.getJSON("seismic.json", function(data){
+			console.log(data);
+			var wigglePlot = g3.plot('.wiggle_plot_div')
+				.height(400)
+				.xDomain([-1000, 1000])
+				.yDomain([0, data.data[0].length])
+				.draw();
+			var wig = g3.wiggle(wigglePlot, data.data)
+				.skip(50)
+				.xMult(0.04)
+				.sampleRate(10)
+				.xTrans(-800)
+				.draw();
+
+			var seismicPlot = g3.plot('.seismic_plot_div')
+				.height(400)
+        .xDomain([0, data.data.length - 1])
+        .yDomain([0, data.data[0].length - 1])
+				.draw();
+
+			var max = data.metadata.max;
+	    var colorScale = d3.scale.linear()
+	    	.domain([-max, 0, max])
+	    	.range(['#FF0000', '#FFF', '#0000FF']);
+
+			var seismic = g3.seismic(seismicPlot, data.data)
+			  .colorScale(colorScale)
+			  .draw();
+
+			// Get a cross section from the 2d array
+		  var arr = [];
+		  for(var i = 0; i < data.data.length; i++){
+		    arr.push(data.data[i][0]);
+		  }
+
+		  // Grab the min and max values from the array
+		  var max = d3.max(arr);
+		  var min = d3.min(arr);
+
+			var horizonPlot = g3.plot('.horizon_plot_div')
+				.height(400)
+        .xDomain([0, data.data.length])
+        .yDomain([min, max])
+				.draw();
+
+			var horizon = g3.horizon(horizonPlot, arr).draw();
+		});
+
 		$scope.scrollTo = function(selector){
         if(jQuery('#' + selector).length == 1){
           console.log(jQuery('#' + selector).offset().top);
